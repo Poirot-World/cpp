0f6f00aa4e781fef104cbf979efcd2574656e770
HuizhiXu <xuhuizhi@hotmail.com>
2020-10-24 18:12:29 +0200

    Initial Commit

---
+++ cppSolutions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppSolutions
+//
+//  Created by Huizhi on 24.10.20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
