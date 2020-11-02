2a34d56a87547f5beb0e18faf988453fccb93d7a
Poirot-World <xuhuizhi612@gmail.com>
2020-11-02 22:41:32 +0100

    c4

---
+++ c4/main.cpp
@@ -0,0 +1,67 @@
+//
+//  main.cpp
+//  c4
+//
+//  Created by Huizhi on 02.11.20.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+using std::vector;
+using std::string;
+
+
+/*
+ //
+int main() {
+
+    cout << "Hello, World!\n";
+    return 0;
+}
+ */
+
+
+/*
+ //4.6 判断奇偶数，用二进制与上1, 1的16进制是0x1，value & 0x1
+int main() {
+    int value;
+    cin >> value;
+    value = value & 1; // a = value & 0x1;
+    cout <<  value << endl;
+    return 0;
+}
+ */
+
+/*
+ //4.5  -86, -18, 11, -2
+int main() {
+    cout << -30 * 3 + 21 / 5 <<endl;
+    cout << -30 + 3 * 21 / 5 <<endl;
+    cout << 30 / 3 + 21 % 5 <<endl;
+    cout << -30 / 3 * 21 % 4 <<endl;
+    return 0;
+}
+
+ */
+
+/*
+ //4.4, 91
+int main() {
+
+    cout << 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2 <<endl;
+    return 0;
+}
+ */
+
+
+
+/*
+ //4.0
+
+int main() {
+   
+    cout << 5+10*20/2 <<endl;
+    return 0;
+}
+ */
---
+++ c5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c5
+//
+//  Created by Huizhi on 02.11.20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
